02a8d8714dbdb1c0a33d14526ef1cc8fb8c4576e
Jan Szumiec <jan.szumiec@gmail.com>
2020-12-26 18:02:20 +0100

    Disable bootscreen, add some custom version info.

--- firmware/V2.0/Marlin-2.0.7.2-SKR-mini-E3-V2.0/Marlin/Version.h
+++ firmware/V2.0/Marlin-2.0.7.2-SKR-mini-E3-V2.0/Marlin/Version.h
@@ -41,7 +41,7 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-//#define STRING_DISTRIBUTION_DATE "2020-07-09"
+#define STRING_DISTRIBUTION_DATE __DATE__
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
@@ -65,7 +65,8 @@
  * The WEBSITE_URL is the location where users can get more information such as
  * documentation about a specific Marlin release.
  */
-//#define WEBSITE_URL "https://marlinfw.org"
+// Ugly hack.
+#define WEBSITE_URL __DATE__
 
 /**
  * Set the vendor info the serial USB interface, if changable
